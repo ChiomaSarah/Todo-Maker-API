6fa1183629dc927244c68284399c609876cb4b06
Osuji Chioma Sarah <osujisarah@gmail.com>
2023-12-15 15:44:11 +0100

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
+  // Enable CORS
+  app.enableCors();
   await app.listen(port);
-  console.log('Hi There! App started on port ' + port);
+  console.log("Hi There! App started on port " + port);
 }
 bootstrap();
